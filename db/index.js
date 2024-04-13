0bcf76dba6c2d6db22c6549a559efc60bdaae52d
Guille1309 <guillermina.abdullatifcoria@gmail.com>
2024-04-12 22:44:56 -0300

    Agregado de estructura básica de módulo de datos(db), creación de carpeta controllers, separación de footer y header en partials

---
+++ db/index.js
@@ -0,0 +1,24 @@
+const datos = {
+    usuario: {
+        nUsuario:,
+        contrasenia:,
+        email:,
+        fechaNacimiento: ,
+        dni: ,
+        fotoPerfil:
+    },
+    productos: [{
+        nombreProducto: , 
+        imagenProducto: ,
+        descripcion: ,
+        comentarios: [{
+            comentador: ,
+            comentario: ,
+            imagenPerfil:  
+        }]
+    },
+        ]
+
+}
+// Revisar los nombres de usuario e imagen en el apartado comentarios de producto //
+module.exports = datos
