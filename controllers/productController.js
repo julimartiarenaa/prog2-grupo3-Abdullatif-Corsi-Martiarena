755472bc07acfe687056dc9edda4c03e5ae12154
Guille1309 <guillermina.abdullatifcoria@gmail.com>
2024-04-15 14:27:22 -0300

    header logueado y controllador de producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,13 +6,9 @@ const productController = {
         res.render("product", {productos: productos})
     },
     addProduct: function (req, res) {
-<<<<<<< HEAD
         let productos = datos.productos
         let usuario = datos.usuario
         res.render("product-add", {productos: productos, usuario: usuario})
-=======
-        res.render("product-add", {productos: productos})
->>>>>>> 84d16cf20de6aed8c4dee4558f121110c7454442
     }
 }
 
