21119a03293edf86c60a75f620158367f86e8e3f
Guille1309 <guillermina.abdullatifcoria@gmail.com>
2024-06-06 12:19:45 -0300

    ruta post del formulario de login, controlador de login, validación de login

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,9 +28,41 @@ const userController = {
     },
     
     login: function (req, res) {
-        let usuario = datos.usuario
-        res.render("login", {usuario: usuario})
-    }
+        res.render("login")
+    },
+    processLogin: function(req, res){
+        let errors = validationResult(req)
+        //return res.send(errors.mapped())
+ 
+        if (errors.isEmpty()) {
+         db.User.findOne({
+             where: [{email:req.body.email}]
+         })
+         .then(function(usuarioEncontrado){
+             //Ponerlos en session.
+             req.session.user = {
+                 email: usuarioEncontrado.email,
+                 userName: usuarioEncontrado.name,
+             }
+             //Preguntar si el usuario tildó el checkbox para recordarlo
+             // return res.send (req.body);
+             if(req.body.recordarme != undefined){
+                 res.cookie('cookieEspecial', 'el dato que quiero guardar', {maxAge: 1000*60*123123123})
+             }
+             //Y si el usuario quiere, agregar la cookie para que lo recuerde.
+             
+             return res.redirect('/');
+         })
+           .catch(function(e){
+             console.log(e);
+         })
+         
+        } else {
+             return res.render("login", {errors: errors.mapped()})
+        }
+          
+     }
 };
+
 
 module.exports = userController;
