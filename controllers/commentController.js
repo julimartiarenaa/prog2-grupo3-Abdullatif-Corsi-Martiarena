fdde37d34c50a33e8e26c8c541efde422acb5ade
Guille1309 <guillermina.abdullatifcoria@gmail.com>
2024-06-18 23:44:01 -0300

    avances add comentario

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,34 +1,51 @@
 const { validationResult } = require("express-validator")
 let db = require("../database/models")
 
-const commentController = { 
-        
-    addComment: function(req, res){
+const commentController = {
+
+    addComment: function (req, res) {
         return res.render("products")
     },
 
-    storeComment: function(req, res){
+    storeComment: function (req, res) {
         let idProducto = req.params.id
-        let errors= validationResult(req)
+        let errors = validationResult(req)
         if (errors.isEmpty()) {
             db.Comentario.create({
                 producto_id: idProducto,
                 comentador_id: req.session.user.id,
                 comentario: req.body.comentario,
-                url_foto_perfil: req.session.user.foto_perfil})
-            .then(function (comment) {
-                return res.redirect("/")
+                url_foto_perfil: req.session.user.foto_perfil
             })
-            .catch(function (errors) {
-                console.log(errors);
+                .then(function (comment) {
+                    return res.redirect("/")
+                })
+                .catch(function (errors) {
+                    console.log(errors);
+                })
+
+        } else {
+            db.Producto.findByPk(idProducto, {
+                include: [
+                    { association: "comentarios" },
+                    { association: "usuarios" }
+
+                ]
             })
-              
-        }else{
-            //return res.render("product", {errors: errors.mapped()})
+                .then(function (producto) {
+                    return db.Usuario.findAll()
+                        .then(function (usuario) {
+                            res.render('product', {errors:errors.mapped(), producto: producto, usuario: usuario })
+                        })
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+
         }
-    } 
-    
-}  
+    }
+
+}
 
 
 module.exports = commentController;
